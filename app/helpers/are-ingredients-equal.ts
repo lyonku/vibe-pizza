a0aa429649c4f680e430da394b448e132ef84361
lyonku <z.pilipenko123@gmail.com>
2024-11-01 18:11:35 +0300

    Ver 2.0.1

--- app/helpers/are-ingredients-equal.ts
+++
@@ -1,9 +0,0 @@
-import { Ingredient } from "@prisma/client";
-
-export const AreIngredientsEqual = (arr1: Ingredient[], arr2: number[]) => {
-  if (arr1.length !== arr2?.length) {
-    return false;
-  }
-
-  return arr1.every((obj1) => arr2.some((id) => obj1.id === id));
-};
